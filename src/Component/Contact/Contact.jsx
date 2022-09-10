131302363655ea95c9f9c1fbdb3dc4ef1d760b91
Belgotech <Belovedobiora2002@gmail.com>
2022-09-09 21:54:44 -0700

    So Far

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -3,7 +3,38 @@ import './Contact.css';
 const Contact = () => {
     return (
        <section id='Contact'>
-        Contact
+        <h5>Get In Touch</h5>
+        <h2>Contact Me</h2>
+        
+        <div className="container contact__container">
+
+            <div className="contact__socials">
+                <article className="socials">
+                    <h3>Eamail</h3>
+                    <h5>Belovedobiora2002@gmail.com</h5>
+                    <a href="#">Send A Message</a>
+                </article>
+                <article className="socials">
+                    <h3>Messenger</h3>
+                    <h5>Be Loved</h5>
+                    <a href="#">Send A Message</a>
+                </article>
+                <article className="socials">
+                    <h3>Whatsapp</h3>
+                    <h5>09064293424</h5>
+                    <a href="#">Send A Message</a>
+                </article>
+            </div>
+
+            <div className="contact__forms">
+                <form action="#">
+                    <input type="text" placeholder='Your Full NAme'/>
+                    <input type="text" placeholder='Your Email'/>
+                    <textarea name="Your Message" id="" cols="40" rows="10" placeholder='Your Message'></textarea>
+                </form>
+                <a href="#" className='btn btn-primary'>Send Message</a>
+            </div>
+        </div>
        </section>
     )
 }
--- src/Component/Testimonial/Testimonial.jsx
+++ src/Component/Testimonial/Testimonial.jsx
@@ -1,26 +1,57 @@
-import './Testimonal.css'
-import avatar1 from '../../assets/avatar1.jpg';
+import "./Testimonal.css";
+import IMG1 from "../../assets/avatar1.jpg";
+import IMG2 from "../../assets/avatar2.jpg";
+import IMG3 from "../../assets/avatar3.jpg";
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    name: "Person One",
+    description:
+      " accusantium, tempora eos aperiam temporibus. Id recusandae quasi ad cupiditate, numquam libero voluptatum inventore itaque, nemo pariatur deleniti laboriosam aut voluptas architecto repellendus omnis quo optio consequatur, dolorem possimus dignissimos.",
+  },
+
+  {
+    id: 2,
+    image: IMG2,
+    name: "Person Two",
+    description:
+      " bccusantium, tempora eos aperiam temporibus. Id recusandae quasi ad cupiditate, numquam libero voluptatum inventore itaque, nemo pariatur deleniti laboriosam aut voluptas architecto repellendus omnis quo optio consequatur, dolorem possimus dignissimos.",
+  },
+
+  {
+    id: 3,
+    image: IMG3,
+    name: "Person Three",
+    description:
+      " cccusantium, tempora eos aperiam temporibus. Id recusandae quasi ad cupiditate, numquam libero voluptatum inventore itaque, nemo pariatur deleniti laboriosam aut voluptas architecto repellendus omnis quo optio consequatur, dolorem possimus dignissimos.",
+  },
+];
 
 const Testimonial = () => {
-    return (
-        <section id='Testimonial'>
-            <h5>Reviews From Clients</h5>
-            <h2>Testimonial</h2>
+  return (
+    <section id="Testimonial">
+      <h5>Reviews From Clients</h5>
+      <h2>Testimonial</h2>
 
-            <div className="container textimonal__container">
-                <div className="textimonal__content">
-
-                    <article className="testimonial">
-                        <div className="textimonial__image">
-                            <img src={avatar1} alt="" />
-                        </div>
-                        <b>Person Name</b>
-                        <p> accusantium, tempora eos aperiam temporibus. Id recusandae quasi ad cupiditate, numquam libero voluptatum inventore itaque, nemo pariatur deleniti laboriosam aut voluptas architecto repellendus omnis quo optio consequatur, dolorem possimus dignissimos.</p>
-                    </article>
+      <div className="container textimonal__container">
+        {data.map(({ id, image, name, description }) => {
+          return (
+            <div key={id} className="textimonal__content">
+              <article className="testimonial">
+                <div className="textimonial__image">
+                  <img src={image} alt={name} />
                 </div>
+                <b>{name}</b>
+                <p> {description}</p>
+              </article>
             </div>
-        </section>
-    )
-}
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Testimonial
+export default Testimonial;
